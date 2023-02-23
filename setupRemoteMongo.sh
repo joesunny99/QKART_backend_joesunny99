0ddfff8970dcaeb468ad50073cc33be29e6db837
Joe <joesunny99@gmail.com>
2023-02-23 11:36:45 +0530

    Final with MongoDB connection

---
+++ setupRemoteMongo.sh
@@ -0,0 +1,3 @@
+# Setup file template to upload data to MongoDB Atlas
+mongoimport --uri "mongodb+srv://joesunny99:criouser123@qkart-node.cqzrav0.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection users --file data/export_qkart_users.json
+mongoimport --uri "mongodb+srv://joesunny99:criouser123@qkart-node.cqzrav0.mongodb.net/qkart?retryWrites=true&w=majority" --drop --collection products --file data/export_qkart_products.json
